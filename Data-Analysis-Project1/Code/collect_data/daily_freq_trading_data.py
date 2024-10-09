c59ba550509f4c5773c6b7ca5cd2d37fac67fd38
KarryRen <KarryRenKai@outlook.com>
2024-10-09 23:38:29 +0800

    add: add all

--- Data-Analysis-Project1/Code/collect_data/daily_freq_trading_data.py
+++ Data-Analysis-Project1/Code/collect_data/daily_freq_trading_data.py
@@ -6,7 +6,14 @@
 
 import akshare as ak
 
-# print(ak.stock_zh_a_spot_em())  # only can
+# ---- Step 1. Get the stock of sh and sz ---- #
+# - sh
+stock_sh_code_df = ak.stock_info_sh_name_code(symbol="主板A股")["证券代码"]
+stock_sh_code_df[""]
+# - sz
+stock_sz = ak.stock_info_sz_name_code(symbol="A股列表")
+stock_sz = stock_sz[stock_sz["板块"] == "主板"]["A股代码"]
+print(len(stock_sz))
 
-stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="000001", period="daily", start_date="20000101", end_date="20240101", adjust="")
-print(stock_zh_a_hist_df.iloc[0, :])
+# stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol="000001", period="daily", start_date="20000101", end_date="20240101", adjust="")
+# print(stock_zh_a_hist_df.iloc[0, :])
--- Data-Analysis-Project1/Code/collect_data/trading_dates.py
+++ Data-Analysis-Project1/Code/collect_data/trading_dates.py
@@ -2,4 +2,23 @@
 # @Time    : 2024/10/9 23:02
 # @Author  : Karry Ren
 
-""""""
+""" Get all trading dates of China from 20000101 to 20231231. """
+
+import akshare as ak
+import datetime
+
+# ---- Step 1. Get all date dataframe ---- #
+trading_dates_df = ak.tool_trade_date_hist_sina()
+
+# ---- Step 2. Define the start and end date ---- #
+START_DATE = datetime.date(2000, 1, 1)
+END_DATE = datetime.date(2024, 1, 1)
+
+# ---- Step 3. Select the interval trading date dataframe and transfer the format ---- #
+trading_dates_df = trading_dates_df[trading_dates_df["trade_date"] >= START_DATE]
+trading_dates_df = trading_dates_df[trading_dates_df["trade_date"] <= END_DATE]
+trading_dates_df["trade_date"] = trading_dates_df["trade_date"].apply(lambda x: x.strftime("%Y%m%d"))
+
+# ---- Step 4. Save the data to `.csv` file ---- #
+trading_dates_df.to_csv("../../Data/trading_dates.csv", index=False)
+print(f"Save successful, total `{len(trading_dates_df)}` trading dates.")
