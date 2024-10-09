3b6624a1dfa4122231fec84b414567dcb2e1cd55
KarryRen <KarryRenKai@outlook.com>
2024-10-10 00:21:57 +0800

    add

--- Data-Analysis-Project1/Code/collect_data/daily_freq_trading_data.py
+++ Data-Analysis-Project1/Code/collect_data/daily_freq_trading_data.py
@@ -21,17 +21,17 @@ stock_sz_code_df["Code"] = stock_sz_code_df["Code"].apply(lambda x: str(x) + ".s
 selected_ssh_code_list, selected_ssz_code_list = [], []
 stock_daily_trading_factor_columns = ["股票代码", "日期", "开盘", "收盘", "最高", "最低", "成交量", "成交额", "振幅", "涨跌幅", "涨跌额", "换手率"]
 # - sh
-# stock_sh_code_list = stock_sh_code_df["Code"].values.tolist()
-# for stock_sh_code in stock_sh_code_list:
-#     stock_sh_daily_hist_df = ak.stock_zh_a_hist(symbol=stock_sh_code[:6], period="daily", start_date="20000101", end_date="20240101", adjust="hfq")
-#     if len(stock_sh_daily_hist_df) > 100:
-#         stock_sh_daily_hist_df["日期"] = stock_sh_daily_hist_df["日期"].apply(lambda x: x.strftime("%Y%m%d"))
-#         stock_sh_daily_hist_df["股票代码"] = stock_sh_daily_hist_df["股票代码"].apply(lambda x: str(x) + ".sh")
-#         stock_sh_daily_hist_df = stock_sh_daily_hist_df[stock_daily_trading_factor_columns]  # adjust column sequence
-#         stock_sh_daily_hist_df.to_csv(f"../../../Data/daily_trading_factors/{stock_sh_code}.csv", index=False, encoding="utf_8_sig")
-#         print(f"Finish stock `{stock_sh_code}` !")
-#         selected_ssh_code_list.append(stock_sh_code)
-# print(selected_ssh_code_list)
+stock_sh_code_list = stock_sh_code_df["Code"].values.tolist()
+for stock_sh_code in stock_sh_code_list:
+    stock_sh_daily_hist_df = ak.stock_zh_a_hist(symbol=stock_sh_code[:6], period="daily", start_date="20000101", end_date="20240101", adjust="hfq")
+    if len(stock_sh_daily_hist_df) > 100:
+        stock_sh_daily_hist_df["日期"] = stock_sh_daily_hist_df["日期"].apply(lambda x: x.strftime("%Y%m%d"))
+        stock_sh_daily_hist_df["股票代码"] = stock_sh_daily_hist_df["股票代码"].apply(lambda x: str(x) + ".sh")
+        stock_sh_daily_hist_df = stock_sh_daily_hist_df[stock_daily_trading_factor_columns]  # adjust column sequence
+        stock_sh_daily_hist_df.to_csv(f"../../../Data/daily_trading_factors/{stock_sh_code}.csv", index=False, encoding="utf_8_sig")
+        print(f"Finish stock `{stock_sh_code}` !")
+        selected_ssh_code_list.append(stock_sh_code)
+print(f"Total are `{len(selected_ssh_code_list)}` in sh !")
 # - sz
 stock_sz_code_list = stock_sz_code_df["Code"].values.tolist()
 for stock_sz_code in stock_sz_code_list:
@@ -43,5 +43,4 @@ for stock_sz_code in stock_sz_code_list:
         stock_sz_daily_hist_df.to_csv(f"../../../Data/daily_trading_factors/{stock_sz_code}.csv", index=False, encoding="utf_8_sig")
         print(f"Finish stock `{stock_sz_code}` !")
         selected_ssz_code_list.append(stock_sz_code)
-        break
-print(selected_ssz_code_list)
+print(f"Total are `{len(selected_ssz_code_list)}` in sz !")
