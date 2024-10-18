e8dc89511a013f0a0831036bea8957af3ebe263d
KarryRen <KarryRenKai@outlook.com>
2024-10-18 21:01:03 +0800

    finish: finish merge code (#6)

--- Data-Analysis-Project1/Code/factor_enginnering/factors_daily_trading/concat_raw_data.py
+++ Data-Analysis-Project1/Code/factor_enginnering/factors_daily_trading/concat_raw_data.py
@@ -11,11 +11,14 @@ import os
 daily_freq_trading_data_file_list = sorted(os.listdir("../../../../Data/daily_trading_factors/raw_data/"))
 concat_df = pd.DataFrame()
 
-# ---- For loop to concat ---- #
+# ---- For loop to collect ---- #
+df_list = []
 for i, data_file in enumerate(daily_freq_trading_data_file_list):
     daily_freq_trading_df = pd.read_csv(f"../../../../Data/daily_trading_factors/raw_data/{data_file}")
-    concat_df = pd.concat([concat_df, daily_freq_trading_df])
-    print(f"finish reading: {i}, {data_file}")
+    assert str(daily_freq_trading_df["Date"].iloc[0]) == "20100104" and str(daily_freq_trading_df["Date"].iloc[-1]) == "20240531"
+    df_list.append(daily_freq_trading_df)
 
+# ---- Concat the df ---- #
+concat_df = pd.concat(df_list)
 print(concat_df)
-concat_df.to_csv("../../../../Data/daily_trading_factors/processed_factors/daily_trading_factors.csv", index=False)
+concat_df.to_csv("../../../../Data/daily_trading_factors/processed_factors/raw_daily_trading_values.csv", index=False)
