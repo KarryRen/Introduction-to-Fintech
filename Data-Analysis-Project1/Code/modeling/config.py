98379f974071a626f3a9c64423131dbd0a9fcc16
KarryRen <KarryRenKai@outlook.com>
2024-10-29 11:18:24 +0800

    add ignore

--- Data-Analysis-Project1/Code/modeling/config.py
+++ Data-Analysis-Project1/Code/modeling/config.py
@@ -8,7 +8,7 @@
 # ********************************** BASIC SETTINGS ********************************** #
 # ************************************************************************************ #
 RANDOM_SEED = [0, 42, 913][0]  # the random seed
-SAVE_PATH = f"exp_factors/rs_{RANDOM_SEED}/"  # the save path of UCI electricity experiments
+SAVE_PATH = f"exp_factors/rs_{RANDOM_SEED}/"  # the save path
 LOG_FILE = SAVE_PATH + f"log_{RANDOM_SEED}.log"  # the log file path
 MODEL_SAVE_PATH = SAVE_PATH + "trained_models/"  # the saving path of models
 IMAGE_SAVE_PATH = SAVE_PATH + "pred_images/"  # the saving path of pred images
@@ -16,7 +16,7 @@ IMAGE_SAVE_PATH = SAVE_PATH + "pred_images/"  # the saving path of pred images
 # ************************************************************************************ #
 # ************************************ FOR DATASET *********************************** #
 # ************************************************************************************ #
-FACTOR_DATA_PATH = "../../../Data"
+FACTOR_DATA_PATH = "../../../../Data"
 TIME_STEPS = 1
 BATCH_SIZE = 1024 * 2
 FACTOR_NUM = 26
